0e4e62adfaaf65c1ac97ed6ae5a669383162e341
hochiang13 <hochiang13@gmail.com>
2020-01-30 11:38:41 +0800

    Added local IP settings.

--- dns_test.sh
+++ dns_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 while true; do
-    dig_result=$(dig op.10.62.164.143.xip.io)
-    lines=$(echo "${dig_result}" | grep 'ANSWER: 1,'| wc -l)
+    dig_result=$(dig op.10.62.164.143.xip.io @10.36.3.192)
+    lines=$(echo "${dig_result}" | grep 'ANSWER: 0,'| wc -l)
+    echo "${dig_result}"
     if [ $lines == "1" ]; then
         echo "${dig_result}" >> test_output.txt
 	echo "-------------" >> test_output.txt
