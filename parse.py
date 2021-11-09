c75c9855204bc0bacb34d275097401617cda032b
Ho-Chiang Ai <hochiang13@gmail.com>
2021-11-09 10:08:15 +0000

    Changes parse script to count SERVFAIL responses.

--- parse.py
+++ parse.py
@@ -4,9 +4,12 @@ with open("test_output.txt") as f:
     line = f.readline()
     answer_flag = False
     counter_hour = -1
+    server_error_flag = False
     while line:
         if ("ANSWER SECTION" in line):
             answer_flag = True
+        if ("SERVFAIL" in line):
+            server_error_flag = True
         if ("WHEN:" in line):
             time = line.split(" ")[5]
             temp_list = time.split(":")
@@ -17,19 +20,24 @@ with open("test_output.txt") as f:
                 counter_minute = minute
                 good = 0
                 bad = 0
+                server_error = 0
             elif (
                 counter_hour != hour or
                 counter_minute != minute
             ):
-                message = counter_hour + ":" + counter_minute + "\t" + str(good) + "\t" + str(bad)
+                message = counter_hour + ":" + counter_minute + "\t" + str(good) + "\t" + str(bad) + "\t" + str(server_error)
                 print(message)
                 counter_hour = hour
                 counter_minute = minute
                 good = 0
                 bad = 0
+                server_error = 0
             if (answer_flag):
                 answer_flag = False
                 good += 1
             else:
                 bad += 1
+            if (server_error_flag):
+                server_error_flag = False
+                server_error += 1
         line = f.readline()
